84568ea90b7a477bde84bf905829fab7a66338c9
gortsh <94749380+gortsh@users.noreply.github.com>
2021-11-23 18:56:24 +0100

    Update server.js

--- server.js
+++ server.js
@@ -8,5 +8,5 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-    console.log('Server on Port ${PORT}');
-});
+    console.log(`Server listening on port ${PORT}`);
+  });
