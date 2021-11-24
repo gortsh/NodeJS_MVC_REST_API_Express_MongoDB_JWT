4bf7535fa737ac7f08a68ee06db4d2997d1f4a4d
gortsh <94749380+gortsh@users.noreply.github.com>
2021-11-24 15:00:06 +0100

    middleware with cors

--- server.js
+++ server.js
@@ -1,18 +1,27 @@
 const express = require('express');
 const dotEnv = require('dotenv');
+const cors = require('cors');
 
 dotEnv.config();
 
 const app = express();
 
-const myMiddleware = (req, res, next) => {
+//cors
+app.use(cors());
+
+
+//request payload middleware
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+/*const myMiddleware = (req, res, next) => {
     console.log('middle');
     next();
 }
-
+*/
 //app.use(myMiddleware);
 
-app.get('/', myMiddleware, (req, res, next) => {
+app.get('/', (req, res, next) => {
     res.send('Test nodemon')
 });
 
